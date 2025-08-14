596c6db2a158720906daf25c8d1ff0a62642f122
Zulay71 <zulaybustos71@gmail.com>
2025-08-14 11:21:49 -0600

    Mi Primer commit en Java

---
+++ IF2000_introducción progra/src/if2000_introducción/progra/IF2000_introducciónProgra.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introducción.progra;
+
+/**
+ *
+ * @author luzma
+ */
+public class IF2000_introducciónProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Data types
+        String name = "Lionel";
+        int age = 40;
+        
+        System.out.println ("Hello World");
+        System.out.println ("Name:"+ name);
+        System.out.println ("Age:"+ age);
+        
+        //Python:
+        //print('Hola Mundo')
+        
+        /*Tipos de datos numéricos
+        int, long, short, float, double, numeric, money, decimal
+        
+        Manejo de flechas:
+        Date
+        
+        Cadenass de caracteres:
+        String, char
+        
+        Obejectos:
+        Object
+        
+        Data Structures:
+        Arrays or Vectors: int[] x;
+                           int[][] y;
+        
+        
+        True/False:
+        boolean
+        
+        */
+        
+    }
+    
+}
