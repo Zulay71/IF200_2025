8b1703ff530e68ef25d01a64c87dee7acab7b6e6
Zulay71 <zulaybustos71@gmail.com>
2025-08-23 15:51:02 -0600

    commit TC1 PracticaEnClase

--- IF2000_introducción progra/src/if2000_introducción/progra/IF2000_introducciónProgra.java
+++ IF2000_introducción progra/src/if2000_introducción/progra/IF2000_introducciónProgra.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package if2000_introducción.progra;
+import logic.Ejercicios_Semana2_PracticaEnClase;
 
 /**
  *
@@ -13,13 +14,42 @@ public class IF2000_introducciónProgra {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {   
+    
+    System.out.println("----------Eejercicio A-----------------");
+    Ejercicios_Semana2_PracticaEnClase eje = new Ejercicios_Semana2_PracticaEnClase();
+    eje.ejercicioA(5);
         
+   System.out.println("----------Ejercicio B------------------");
+   eje.ejercicioB(12);
+   
+   System.out.println("----------Ejercicio C------------------");
+   eje.ejercicioC();
+   
+   System.out.println("----------Ejercicio D------------------");
+   eje.ejercicioD();
+   
+   System.out.println(" ");
+   
+   System.out.println("----------Ejercicio E------------------");
+   eje.ejercicioE();
+    
+   System.out.println("----------Ejercicio F------------------");
+   eje.ejercicioF();
+    
+   System.out.println("----------Piramide quiz 2--------------");
+   // Crear instancia de la clase y llamar al método de pirámide  
+    Ejercicios_Semana2_PracticaEnClase ej;
+    ej = new Ejercicios_Semana2_PracticaEnClase();
+    ej.quiz2_B_piramide (5);   
+   
+
+// TODO code application logic here
+       
         //Data types
         String name = "Lionel";
         int age = 40;
-        
+        System.out.println("-----------Ejemplo-------------");
         System.out.println ("Hello World");
         System.out.println ("Name:"+ name);
         System.out.println ("Age:"+ age);
@@ -48,6 +78,7 @@ public class IF2000_introducciónProgra {
         boolean
         
         */
+     
         
     }
     
---
+++ IF2000_introducción progra/src/logic/Ejercicios_Semana2_PracticaEnClase.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+/**
+ *
+ * @author luzma
+ */
+public class Ejercicios_Semana2_PracticaEnClase {
+
+    public Ejercicios_Semana2_PracticaEnClase() {
+    }
+ 
+ public void quiz2_B_piramide(int n){
+ 
+for (int i = 1; i <= n ; i++) {
+//controla los espaios vacios
+ for (int j = 1; j <= (n - i) ; j++){
+  System.out.print(" ");
+ }//endForInterno_A
+ //controla los *s de la piramide
+ for (int k = 1; k <= (2 * i - 1); k++){
+  System.out.print("*");
+ }//endForInterno B
+ System.out.println();
+ }//endForExterno  
+ }//endMetodo
+ 
+ public void ejercicioA(int n){
+ for (int i = 1; i <= n; i++) {
+ for (int j = 1; j <= n; j++) {
+ // Imprimir * en los bordes
+ if (i == 1 || i == n || j == 1 || j == n) {
+ System.out.print("*");
+ } else {
+System.out.print(" ");
+}
+}
+System.out.println();     
+}     
+}
+public void ejercicioB(int n){
+
+ for (int i = 12; i >= 1; i--) {
+ for (int j = 1; j <= i; j++) {
+ System.out.print("*");
+ }
+System.out.println();
+} 
+}
+
+public void ejercicioC(){
+int n = 10;
+    
+ for (int i = n; i >= 1; i--) {
+ for (int j = 0; j < n - i; j++) {
+ System.out.print(" ");
+ }
+ for (int k = 0; k < i; k++) {
+ System.out.print("*");
+ }
+ System.out.println();
+}
+}
+public void ejercicioD(){
+    
+Scanner sc = new Scanner(System.in);
+int[] numeros = new int[10];
+
+System.out.println("Ingrese 10 numeros enteros:");
+for (int i = 0; i < 10; i++) {
+System.out.print("Numero " + (i + 1) + ": ");
+numeros[i] = sc.nextInt();
+}
+Arrays.sort(numeros);
+
+System.out.println("\nArreglo ordenado ascendentemente:");
+for (int num : numeros) {
+System.out.print(num + " ");
+}  
+}
+public void ejercicioE() {
+ 
+Integer[] numeros = new Integer[100];
+ Random rand = new Random();
+
+ // Generar 100 números aleatorios 
+ for (int i = 0; i < 100; i++) {
+ numeros[i] = rand.nextInt(1000) + 1;
+ }
+
+// Ordenar descendentemente
+Arrays.sort(numeros, Collections.reverseOrder());
+
+// Imprimir el arreglo ordenado
+System.out.println("Arreglo ordenado descendentemente:");
+for (int i = 0; i < numeros.length; i++) {
+System.out.print(numeros[i] + " ");
+if ((i + 1) % 10 == 0) {
+System.out.println(); 
+}
+}
+}
+public void ejercicioF(){
+char[][] tablero = new char[8][8];
+for (int i = 0; i < 8; i++)
+for (int j = 0; j < 8; j++)
+tablero[i][j] = ' ';
+
+ for (int i = 0; i < 3; i++)
+ for (int j = 0; j < 8; j++)
+ if ((i + j) % 2 == 1) tablero[i][j] = 'N';
+
+for (int i = 5; i < 8; i++)
+for (int j = 0; j < 8; j++)
+if ((i + j) % 2 == 1) tablero[i][j] = 'R';
+
+System.out.println("\nTablero");
+System.out.print("  ");
+        for (int i = 1; i <= 8; i++) System.out.print(i + " ");
+System.out.println();
+
+for (int i = 0; i < 8; i++) {
+System.out.print((char)('A'+i) + " ");
+for (int j = 0; j < 8; j++) System.out.print(tablero[i][j] + " ");
+System.out.println();
+}
+
+}
+
+}
+
+
+
+
